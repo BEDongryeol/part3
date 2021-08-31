02d81c7f7401cca4c2da45c8e37970580c7dbefe
BEDongryeol <ehdfuf53@gmail.com>
2021-08-31 17:07:52 +0900

    feat: Override getAccountInfo method
    - Parameter Type : SavingAccount
    - Prints category, account number, owner, balance, goal amount

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -26,13 +26,14 @@ public class SavingAccount extends Account{
     }
 
     //TODO: GoalAmount getter 구현
-
     public BigDecimal getGoalAmount() {
         return goalAmount;
     }
 
     //TODO: getAccountInfo를 재정의하여 "목표 금액"도 노출해줍니다.
-    public void getAccountInfo(Account account) {
+    public void getAccountInfo(SavingAccount account) {
         //TODO
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액: %s",
+                account.category, account.accNo, account.owner, account.balance, account.goalAmount);
     }
 }
