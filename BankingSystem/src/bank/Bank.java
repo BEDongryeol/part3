39954a1df5563534a50820c551d8dc0e65124392
BEDongryeol <ehdfuf53@gmail.com>
2021-09-01 19:10:48 +0900

    feat: Added 'createAccount()' method
    - Declare account(Account), owner(String), balance(BigDecimal)
    - When this method called, seq(account number) get 1 added
    - Get name, amount from Scanner and put them into parameter of Constructor
    - Throw exceptions when name or account is null or unsuitable

--- BankingSystem/src/bank/Bank.java
+++ BankingSystem/src/bank/Bank.java
@@ -64,7 +64,7 @@ public class Bank{
         }
     }
 
-    public void deposit(){
+    public void deposit() {
         //TODO: 입금 메서드 구현
         // 존재하지 않는 계좌이면 다시 물어보기
         int count = 0;
@@ -73,7 +73,7 @@ public class Bank{
 
         while (count == 1 || trial > 5) {
             trial++;
-            System.out.println("\n입금하시려는 계좌번호를 입력해주세요. 남은 시도 가능 횟수는 " + (5-trial) + "회입니다.");
+            System.out.println("\n입금하시려는 계좌번호를 입력해주세요. 남은 시도 가능 횟수는 " + (5 - trial) + "회입니다.");
             Scanner scanner = new Scanner(System.in);
             String accNo = scanner.next();
 
@@ -95,18 +95,50 @@ public class Bank{
         Scanner scanner = new Scanner(System.in);
         BigDecimal amount = scanner.nextBigDecimal();
         account.deposit(amount);
+    }
 
     public Account createAccount() throws InputMismatchException {
         //TODO: 계좌 생성하는 메서드 구현
+        Account account = null;
+        String owner = null;
+        BigDecimal balance;
+
         try {
-            // 계좌번호 채번
-            // 계좌번호는 "0000"+증가한 seq 포맷을 가진 번호입니다.
-            //TODO
-            System.out.printf("\n%s님 계좌가 발급되었습니다.\n", owner);
-            return account;
-        }catch (){
+        // 계좌번호 채번
+        // 계좌번호는 "0000"+증가한 seq 포맷을 가진 번호입니다.
+        seq++;
+        String accNo = df.format(seq);
+
+        // 이름 등록
+        System.out.println("이름을 입력하세요");
+        Scanner scan = new Scanner(System.in);
+        owner = scan.next();
+
+        // 입금액
+        System.out.println("입금액을 입력하세요");
+        Scanner scan1 = new Scanner(System.in);
+        balance = scan1.nextBigDecimal();
+
+        // 계좌 생성
+        account = new Account(accNo, owner, balance);
+
+
+        }catch (InputMismatchException e){
             //TODO: 오류 throw
+            if (owner == null) {
+                throw new InputMismatchException("이름은 null이 될 수 없습니다.");
+            }
+            if (owner.length() <= 1) {
+                throw new InputMismatchException("이름이 너무 짧습니다.");
+            }
+            if (account == null) {
+                throw new InputMismatchException("계좌 생성에 실패하였습니다.");
+            }
+            if owner.matches("[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣+]"){
+                throw new InputMismatchException("올바른 이름이 아닙니다.");
+            }
         }
+        return account;
     }
 
     public Account findAccount(String accNo){
