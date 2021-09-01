a2dc70ec0111a8be4f52eded845e354418cf9f77
BEDongryeol <ehdfuf53@gmail.com>
2021-09-01 13:34:52 +0900

    feat: Created CentralBank.java
    - Designed as Singleton Pattern
    - Variable
    1. accountList (Type: ArrayList<Account>)
    2. BANK_NAME (Type: String)
    - Method
    1. getAccountList() (return instance)
    2. setAccountList() (set accountList)

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -7,11 +7,27 @@ import java.util.ArrayList;
 public class CentralBank {
     //은행시스템의 계좌들을 관리하는 중앙은행 클래스입니다.
     //TODO: 싱글톤 패턴으로 설계합니다.
+    private static final CentralBank instance = new CentralBank();
+
+    private CentralBank(){}
+
     //TODO: accountList(Account로 이루어진 ArrayList)
+    public static ArrayList<Account> accountList = new ArrayList<>();
+
     //TODO: BANK_NAME(은행명)
+    public static final String BANK_NAME = "동렬은행";
 
     //TODO: getInstance 함수
+    public static CentralBank getInstance() {
+        return instance;
+    }
 
     //TODO: accountList getter/setter
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
 
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
